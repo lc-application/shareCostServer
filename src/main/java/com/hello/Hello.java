d7a1ae5431e8550136c8a5ae854b1d9278313efa
Xuanyu Li <li.xua@husky.neu.edu>
2019-03-09 15:19:08 -0500

    try hello

--- src/main/java/com/hello/Hello.java
+++ src/main/java/com/hello/Hello.java
@@ -1,4 +1,49 @@
 package com.hello;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="hello")
 public class Hello {
+    @Id
+    @GeneratedValue
+    private int id;
+    public int getId() {
+        return id;
+    }
+    private String message;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /*
+     * Read function
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /*
+     * Update function
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /*
+     * Create function within an initialized value
+     */
+    public Hello(String message) {
+        this.message = message;
+    }
+
+    /*
+     * Create function
+     */
+    public Hello() {
+    }
+
 }
--- src/main/java/com/hello/HelloController.java
+++ src/main/java/com/hello/HelloController.java
@@ -1,14 +1,52 @@
 package com.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class HelloController {
-    @RequestMapping("/")
-    @ResponseBody
-    String home(){
-        return "Hello World!";
+
+    @Autowired
+    private HelloRepository helloRepository;
+
+    @RequestMapping("/api/hello/insert")
+    public Hello insertHelloObject() {
+        Hello obj = new Hello("Hello my team!");
+        helloRepository.save(obj);
+        helloRepository.flush();
+        return obj;
     }
+
+    @RequestMapping("/api/hello/insert/{msg}")
+    public Hello insertMessage(@PathVariable("msg") String message) {
+        Hello obj = new Hello(message);
+        helloRepository.save(obj);
+        helloRepository.flush();
+        return obj;
+    }
+
+    @RequestMapping("/api/hello/select/all")
+    public List<Hello> selectAllHelloObjects() {
+        List<Hello> hellos =
+                (List<Hello>)helloRepository.findAll();
+        return hellos;
+    }
+
+    @RequestMapping("/api/hello/string")
+    public String sayHello() {
+        return "Hello Team!";
+    }
+
+    @RequestMapping("/api/hello/object")
+    public Hello sayHelloObject() {
+        Hello obj = new Hello("Hello Team!");
+        return obj;
+    }
+
 }
